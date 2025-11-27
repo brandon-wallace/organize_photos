257dd61e3ce7d9e7d8452e5ff3e15669b1533727
Brandon Wallace <17948072+brandon-wallace@users.noreply.github.com>
2025-11-27 11:06:37 -0500

    Update organize_photos.go
    improve error messages
    add extensions jpeg
    check for existing files to avoid overwriting them
    improve summary at the end

--- organize_photos.go
+++ organize_photos.go
@@ -11,65 +11,109 @@ import (
 
 const (
 	format    = "2006/01/02"
-	extension = "*.jpg"
+)
+
+var (
+	extensions = []string{"*.jpg", "*.JPG", "*.jpeg", "*.JPEG"}
 )
 
 // makeNewDirectory makes a directory and all parent directories
-func makeNewDirectory(directory string) {
+func makeNewDirectory(directory string) error {
 	err := os.MkdirAll(directory, 0o755)
 	if err != nil {
 		log.Fatal(err)
-		return
+		return fmt.Errorf("failed to create directory %s: %w", directory, err)
 	}
+
+	return nil
 }
 
 // getFiles lists all JPG files in the current directory
 func getFiles() ([]string, error) {
-	files, err := filepath.Glob(extension)
-	if err != nil {
-		return nil, err
+	files := make([]string, 0)
+	for _, ext := range extensions {
+		matches, err := filepath.Glob(ext)
+		if err != nil {
+			return nil, fmt.Errorf("failed to glob pattern %s: %w", ext, err)
+		}
+		files = append(files, matches...)
 	}
+
 	return files, nil
 }
 
 // readMetadata extracts the datetime information from the metadata
-func readMetadata(image string) string {
+func readMetadata(image string) (string, error) {
 	img, err := os.Open(image)
 	if err != nil {
-		log.Fatal(err)
+		return "", fmt.Errorf("failed to open image %s %w", image, err)
 	}
 	defer img.Close()
 
 	metadata, err := exif.Decode(img)
 	if err != nil {
-		log.Fatal(err)
+		return "", fmt.Errorf("failed to decode EXIF data %s %w", image, err)
 	}
-	datetime, _ := metadata.DateTime()
+	datetime, err := metadata.DateTime()
+	if err != nil {
+		return "", fmt.Errorf("failed to get datetime from %s %w", image, err)
+	}
 
-	return datetime.Format(format)
+	return datetime.Format(format), nil
 }
 
 // moveFile moves a file from a source to a destination
 func moveFile(file string, destination string) error {
+	filename := filepath.Base(file)
+	path := filepath.Join(destination, filename)
+
+	if _, err := os.Stat(path); err == nil {
+		return fmt.Errorf("file already exists: %s", path)
+	}
+
 	return os.Rename(file, fmt.Sprintf("%s/%s", destination, file))
 }
 
 func main() {
-	fmt.Println("Organizing JPG Files")
+	fmt.Println("\n Organizing JPG Files...")
+
 	files, err := getFiles()
 	if err != nil {
 		log.Fatal("error: something has gone wrong")
 	}
+	
+	if len(files) == 0 {
+		fmt.Println("No image files to process.")
+		return
+	}
 
-	count := 0
+	errorCount := 0
+	successCount := 0
+
 	for _, img := range files {
-		directory := readMetadata(img)
-		makeNewDirectory(directory)
-		err := moveFile(img, directory)
+		directory, err := readMetadata(img)
+		if err != nil {
+			log.Printf("error getting date for %s: %v", img, err)
+			errorCount++
+			continue
+		}
+
+		if err := makeNewDirectory(directory); err != nil {
+			errorCount++
+			fmt.Printf("error creating directory for %s: %v", img, err)
+			continue
+		}
+		err = moveFile(img, directory)
 		if err != nil {
 			log.Fatal("error: ", err)
 		}
-		count++
+
+		successCount++
 	}
-	fmt.Printf("%d files moved.\n", count)
+
+	fmt.Printf("\n Organization complete.\n")
+	fmt.Printf(" Successfully moved files: %d\n", successCount)
+	fmt.Printf(" Errors: %d\n", errorCount)
+	fmt.Printf(" Total files moved: %d.\n", len(files))
 }
+
